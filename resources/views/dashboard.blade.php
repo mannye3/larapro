0a41241c261814d083270caab756fc6a6380f6bb
unknown <you@example.com>
2019-10-10 16:57:06 +0100

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,34 @@
                     @endif
                     <a href="/posts/create" class="btn btn-primary">Create Post</a>
                    <h3> Your Blog Post</h3>
+                   @if(count($posts)>0)
+                   <table class="table table-striped">
+                    <tr>
+                            <th>Title</th>
+                            <th></th>
+                            <th></th>
+                    </tr>
+                    <tr>
+                        @foreach($posts as $post)
+
+                <th>{{$post->title}}</th>
+                <th><a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a> </th>
+                    <th><form action="{{ route('posts.destroy',$post->id) }}" method="POST" style="align: float-right;">
+                        <input type="hidden" name="_method" value="DELETE">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <button class="btn btn-danger" type="submit"> Delete </button>
+                </form></th>
+
+                    </tr>
+
+
+                        
+                    @endforeach
+
+                   </table>
+                   @else
+                   <p>You have no post</p>
+                   @endif
                 </div>
             </div>
         </div>
